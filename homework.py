8e7b8ac238f06d0ca4ef78dbc6a958ef8e51b078
Sergei Anisimov <seroja.92@mail.ru>
2022-12-16 13:27:42 +0300

    исправлено по замечаниям

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 from dataclasses import asdict, dataclass
-from typing import Any, List
+from typing import List
 
 
 @dataclass
@@ -24,15 +24,16 @@ class InfoMessage:
 
 class Training:
     """Базовый класс тренировки."""
-    LEN_STEP: float = 0.65  # константа для обозначения длины одного шага.
-    M_IN_KM: int = 1000  # константа количества метров в километре.
-    MIN_IN_H: int = 60  # константа количества минут в часе.
+    LEN_STEP: float = 0.65
+    M_IN_KM: int = 1000
+    MIN_IN_H: int = 60
 
     def __init__(self,
                  action: int,
                  duration: float,
                  weight: float,
                  ) -> None:
+        """Константа для перевода значений из метров в километры"""
         self.action = action
         self.duration = duration
         self.weight = weight
@@ -52,7 +53,7 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(type(self).__name__,
+        return InfoMessage(self.__class__.__name__,
                            self.duration,
                            self.get_distance(),
                            self.get_mean_speed(),
@@ -62,10 +63,11 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-    CALORIES_MEAN_SPEED_MULTIPLIER: int = 18  # для расчета калорий
-    CALORIES_MEAN_SPEED_SHIFT: float = 1.79  # для расчета калорий
+    CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
+    CALORIES_MEAN_SPEED_SHIFT: float = 1.79
 
     def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий."""
         return ((self.CALORIES_MEAN_SPEED_MULTIPLIER
                 * self.get_mean_speed()
                 + self.CALORIES_MEAN_SPEED_SHIFT)
@@ -77,16 +79,17 @@ class Running(Training):
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    CALORIES_WEIGHT_MULTIPLIER: float = 0.035  # для расчета калорий.
-    CALORIES_SPEED_HEIGHT_MULTIPLIER: float = 0.029  # для расчета калорий.
-    KMH_IN_MSEC: float = 0.278  # метры в секунду в 1м км/ч.
-    CM_IN_M: int = 100  # сантиметры в 1 метре.
+    CALORIES_WEIGHT_MULTIPLIER: float = 0.035
+    CALORIES_SPEED_HEIGHT_MULTIPLIER: float = 0.029
+    KMH_IN_MSEC: float = 0.278
+    CM_IN_M: int = 100
 
     def __init__(self,
                  action: int,
                  duration: float,
                  weight: float,
                  height: int) -> None:
+        """Получить количество затраченных калорий."""
         super().__init__(action, duration, weight)
         self.height = height
 
@@ -116,6 +119,7 @@ class Swimming(Training):
         self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
+        """Получить среднюю скорость движения."""
         return (self.length_pool
                 * self.count_pool
                 / self.M_IN_KM
@@ -124,20 +128,19 @@ class Swimming(Training):
     def get_spent_calories(self) -> float:
         return ((self.get_mean_speed() + self.COEF_SWIM)
                 * self.COEF_SWIM2
-                * self.weight
-                * self.duration)
+                * self.weight)
 
 
 def read_package(workout_type: str, data: List[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    TRAINIG_TYPES: Any = {
+    training_types = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking
     }
-    if TRAINIG_TYPES.values() not in TRAINIG_TYPES:
-        raise ValueError('выбрать допустимый класс тренировки')
-    return TRAINIG_TYPES[workout_type](*data)
+    if workout_type not in training_types:
+        raise ValueError('Неверный тип тренировки')
+    return training_types[workout_type](*data)
 
 
 def main(training: Training) -> None:
